832e7444babfcb0683c471f64b7a78eb79016d0b
Bence Beregszászi <cl3zhy@std.uni-eszterhazy.hu>
2022-09-18 17:22:29 +0200

    Technical fix

--- Spring/src/main/java/ekke/spring/configuration/MethodSecurityConfig.java
+++ Spring/src/main/java/ekke/spring/configuration/MethodSecurityConfig.java
@@ -1,12 +1,27 @@
 package ekke.spring.configuration;
 
+import lombok.SneakyThrows;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableAutoConfiguration
+@EnableWebSecurity
 @EnableGlobalMethodSecurity(
         prePostEnabled = true
 )
-public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
+public class MethodSecurityConfig {
+
+    @Bean
+    @SneakyThrows
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) {
+        httpSecurity.csrf().disable();
+        return httpSecurity.build();
+    }
 }
