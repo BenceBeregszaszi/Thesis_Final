031991be2473d6a52d0cfce2ec6e0089f9724333
Bence Beregszászi <cl3zhy@std.uni-eszterhazy.hu>
2022-10-16 16:29:53 +0200

    Add activities

---
+++ MobileApp/app/src/main/java/ekke/mobile/mobileapp/Reservations.kt
@@ -0,0 +1,11 @@
+package ekke.mobile.mobileapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Reservations : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reservations)
+    }
+}
