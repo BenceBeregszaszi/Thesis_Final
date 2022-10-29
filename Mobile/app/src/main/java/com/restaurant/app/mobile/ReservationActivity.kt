6cf3fc2ea568df6846068e31fc8edf8e68ce46c5
Bence Beregszászi <cl3zhy@std.uni-eszterhazy.hu>
2022-10-29 16:40:25 +0200

    Generate Activities

---
+++ Mobile/app/src/main/java/com/restaurant/app/mobile/ReservationActivity.kt
@@ -0,0 +1,11 @@
+package com.restaurant.app.mobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReservationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reservation)
+    }
+}
